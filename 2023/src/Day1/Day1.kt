a6ac63222e286d428d78fb0207fdd828a0fa5162
NKoepf <koepf.n@gmail.com>
2023-12-01 13:06:14 +0100

    removed some commentary

--- 2023/src/Day1/Day1.kt
+++ 2023/src/Day1/Day1.kt
@@ -41,119 +41,99 @@ fun replaceWrittenDigits(line: String): String {
     for(i: Int in 1..line.length){
         val subLine = line.subSequence(0, i).toString()
         if(subLine[subLine.length -1].isDigit()) break
-        println("sub: $subLine")
         if(subLine.contains("one")){
             newLine = newLine.replaceFirst("one", "1")
             break
         }
         if(subLine.contains("two")){
             newLine = newLine.replaceFirst("two", "2")
-            println(newLine)
             break
         }
         if(subLine.contains("three")){
             newLine = newLine.replaceFirst("three", "3")
-            println(newLine)
             break
         }
         if(subLine.contains("four")){
             newLine = newLine.replaceFirst("four", "4")
-            println(newLine)
             break
         }
         if(subLine.contains("five")){
             newLine = newLine.replaceFirst("five", "5")
-            println(newLine)
             break
         }
         if(subLine.contains("six")){
             newLine = newLine.replaceFirst("six", "6")
-            println(newLine)
             break
         }
         if(subLine.contains("seven")){
             newLine = newLine.replaceFirst("seven", "7")
-            println(newLine)
             break
         }
         if(subLine.contains("eight")){
             newLine = newLine.replaceFirst("eight", "8")
-            println(newLine)
             break
         }
         if(subLine.contains("nine")){
             newLine = newLine.replaceFirst("nine", "9")
-            println(newLine)
             break
         }
 
-        println(newLine)
     }
+
     println("first number replaced $newLine")
 
     for(i: Int in 0..newLine.length - 1){
         val subLine = newLine.subSequence(newLine.length - i - 1, newLine.length).toString()
-        println("sub: $subLine")
         if(subLine[0].isDigit()) break
 
         if(subLine.contains("one")){
             val startIndex = newLine.lastIndexOf("one")
             newLine = newLine.replaceRange(startIndex, startIndex + 3, "1")
-            println(newLine)
             break
         }
         if(subLine.contains("two")){
             val startIndex = newLine.lastIndexOf("two")
             newLine = newLine.replaceRange(startIndex, startIndex + 3, "2")
-            println(newLine)
             break
         }
         if(subLine.contains("three")){
             val startIndex = newLine.lastIndexOf("three")
             newLine = newLine.replaceRange(startIndex, startIndex + 5, "3")
-            println(newLine)
             break
         }
         if(subLine.contains("four")){
             val startIndex = newLine.lastIndexOf("four")
             newLine = newLine.replaceRange(startIndex, startIndex + 4, "4")
-            println(newLine)
             break
         }
         if(subLine.contains("five")){
             val startIndex = newLine.lastIndexOf("five")
             newLine = newLine.replaceRange(startIndex, startIndex + 4, "5")
-            println(newLine)
             break
         }
         if(subLine.contains("six")){
             val startIndex = newLine.lastIndexOf("six")
             newLine = newLine.replaceRange(startIndex, startIndex + 3, "6")
-            println(newLine)
             break
         }
         if(subLine.contains("seven")){
             val startIndex = newLine.lastIndexOf("seven")
             newLine = newLine.replaceRange(startIndex, startIndex + 5, "7")
-            println(newLine)
             break
         }
         if(subLine.contains("eight")){
             val startIndex = newLine.lastIndexOf("eight")
             newLine = newLine.replaceRange(startIndex, startIndex + 5, "8")
-            println(newLine)
             break
         }
         if(subLine.contains("nine")){
             val startIndex = newLine.lastIndexOf("nine")
             newLine = newLine.replaceRange(startIndex, startIndex + 4, "9")
-            println(newLine)
             break
         }
 
-        println(newLine)
     }
-    println(newLine)
+    println("replaces last number word $newLine")
     return newLine
 }
 
